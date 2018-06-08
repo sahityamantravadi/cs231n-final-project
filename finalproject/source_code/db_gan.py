0847f749f182f6059c1584ea607749b47c7f0829
Sahitya Mantravadi <sahitya.mantravadi@gmail.com>
2018-06-08 04:11:08 +0000

    done!

--- finalproject/source_code/db_gan.py
+++ finalproject/source_code/db_gan.py
@@ -20,7 +20,7 @@ EPS = 1e-12
 ngf = 32 #number of generator filters in first conv layer
 ndf = 32 #number of discriminator filters in first conv layer
 seed = 123
-lr = 0.00001 #initial learning rate for adam
+lr = 0.0001 #initial learning rate for adam
 beta1 = 0.5 #momentum term of adam"
 dropout_g = 0.3
 dropout_d = 0.3
@@ -31,10 +31,10 @@ Site_weight = 2.0
 #test_dir = '/home/smantra/finalproject/logs/debiasing_GAN/test'
 #label_file = '/home/smantra/finalproject/logs/debiasing_GAN/images/image_labels.txt'
 
-test_dir = '/home/smantra/finalproject/logs/debiasing_GAN/test_skip_conn'
-train_dir = '/home/smantra/finalproject/logs/debiasing_GAN/train_skip_conn'
-label_file = '/home/smantra/finalproject/logs/debiasing_GAN/images/skip_conn_image_labels.txt'
-
+test_dir = '/home/smantra/finalproject/logs/debiasing_GAN/test_with_skip_and_conv'
+train_dir = '/home/smantra/finalproject/logs/debiasing_GAN/train_with_skip_and_conv'
+label_file = '/home/smantra/finalproject/logs/debiasing_GAN/images/skip_and_conv_image_labels.txt'
+test_out_file = '/home/smantra/finalproject/logs/debiasing_GAN/test_output.txt'
 
 def save_batch(pre, qc_lab, site_lab, post, qc_pred, site_pred, epoch, step, lab_file):
     im_base = 'epoch{}_step{}'.format(epoch, step)
@@ -52,6 +52,14 @@ def save_batch(pre, qc_lab, site_lab, post, qc_pred, site_pred, epoch, step, lab
             imnum_base = '{}_img{}'.format(im_base, i)
             lf.write(imnum_base + '\t' + str(qc_lab[i]) + '\t' + str(site_lab[i]) + '\t' + str(qc_pred[i]) + '\t' + str(site_pred[i]) + '\n')
     print('Saved images for epoch {}, step {}'.format(epoch, step))
+
+def save_preds(batch_num, qc_lab, site_lab, qc_pred, site_pred, lab_file):
+    for i in range(batch_size):
+        with open(lab_file, "a") as lf:
+            imnum_base = 'img{}'.format(batch_num*batch_size + i)
+            lf.write(imnum_base + '\t' + str(qc_lab[i]) + '\t' + str(site_lab[i]) + '\t' + str(qc_pred[i]) + '\t' + str(site_pred[i]) + '\n')
+    print('Saved labels for test step {}'.format(batch_num))
+
 
 def lrelu(x, a=0.1):
     return tf.maximum(a*x, x)
@@ -74,63 +82,45 @@ def G_conv_transpose(batch_input, out_channels, val=True):
 def batchnorm(batch_input, is_training):
     return tf.layers.batch_normalization(batch_input, epsilon=1e-5, momentum=0.1, training=is_training)
 
-def pad_up_to(t, max_in_dims, constant_values):
-    #https://stackoverflow.com/questions/42334646/tensorflow-pad-unknown-size-tensor-to-a-specific-size
-     s = tf.shape(t)
-     paddings = [[0, m-s[i]] for (i,m) in enumerate(max_in_dims)]
-     return tf.pad(t, paddings, 'CONSTANT', constant_values=constant_values)
-
 def generator(G_in, training):
     with tf.variable_scope("generator"):
         layers = []
         G_in = tf.expand_dims(G_in, -1)
         layers.append(G_in)
+        
         # encoder_1
         with tf.variable_scope("encoder_1"):
             conv_out1 = G_conv(G_in, ngf)
-            #rectified = lrelu(conv_out, 0.1)
             layers.append(conv_out1)
+        
         # encoder_2
         with tf.variable_scope("encoder_2"):
             rectified2 = lrelu(layers[-1], 0.1)
             conv_out2 = G_conv(rectified2, ngf*2)
             normalized2 = tf.cond(training, lambda : batchnorm(conv_out2, True), lambda : batchnorm(conv_out2, False))
             layers.append(normalized2)
-        """
-        with tf.variable_scope("conv"):
-            output = D_conv(layers[-1], ngf/2)
-            #rectified = tf.tanh(output)
-            rectified = lrelu(output, 0.1)
-            layers.append(rectified)
-            
-            output1 = D_conv(layers[-1], ngf/4, False)
-            #rectified1 = lrelu(output1, 0.1)
-            rectified1 = tf.tanh(output1)
-            normalized1 = tf.cond(training, lambda : batchnorm(rectified1, True), lambda : batchnorm(rectified1, False))
-            layers.append(normalized1)
-            
-            output2 = D_conv(layers[-1], 1, False)
-            rectified2 = lrelu(output2, 0.1)
-            #rectified2 = tf.tanh(output2)
-            normalized2 = tf.cond(training, lambda : batchnorm(rectified2, True), lambda : batchnorm(rectified2, False))
-            kept2 = tf.cond(training, lambda : tf.nn.dropout(normalized2, keep_prob=1 - dropout_g), lambda : normalized2)
-            layers.append(kept2)
-        """
+        
         with tf.variable_scope("decoder_2"):
             rectified3 = tf.nn.relu(layers[-1])
             convt_out3 = G_conv_transpose(rectified3, ngf)
             normalized3 = tf.cond(training, lambda : batchnorm(convt_out3, True), lambda : batchnorm(convt_out3, False))
             kept3 = tf.cond(training, lambda : tf.nn.dropout(normalized3, keep_prob=1 - dropout_g), lambda : normalized3)
-            layers.append(kept3)
+            layer4 = D_conv(kept3, ngf/2, False)
+            layers.append(layer4)
 
         with tf.variable_scope("decoder_1"):
-            #padded4 = pad_up_to(layers[-1], (4, 52, 63, 54, ngf), 0)
             padded4 = tf.pad(layers[-1], [[0,0], [0,0], [1,0], [0,0], [0,0]])
             dec_input4 = tf.concat([padded4, conv_out1], axis=4) #implementing skip connection
             rectified4 = tf.nn.relu(dec_input4)
-            convt_out4 = G_conv_transpose(rectified4, 1)
+            convt_out4 = G_conv_transpose(rectified4, ngf)
             output = tf.tanh(convt_out4)
             layers.append(output)
+        last_input = tf.concat([layers[-1], G_in], axis=4) #skip connection
+        last_layer = D_conv(last_input, 1, False)
+        last_layer = tf.tanh(last_layer)
+        normalized = tf.cond(training, lambda : batchnorm(last_layer, True), lambda : batchnorm(last_layer, False))
+        kept = tf.cond(training, lambda : tf.nn.dropout(normalized, keep_prob=1 - dropout_g), lambda : normalized)
+        layers.append(kept)
         
         squeezed = tf.squeeze(layers[-1])
         layers.append(squeezed)
@@ -157,23 +147,18 @@ def site_discriminator(D_input, training):
                 out_channels = ndf/(2*(i+1))
                 convolved = D_conv(layers[-1], out_channels)
                 pooled = D_max_pool(convolved)
-                #normalized = batchnorm(pooled, training)
                 normalized = tf.cond(training, lambda : batchnorm(pooled, True), lambda : batchnorm(pooled, False))
                 out = lrelu(normalized, 0.1)
                 kept = tf.cond(training, lambda : tf.nn.dropout(out, keep_prob=1 - dropout_d), lambda : out)
-                #if training:
-                #    out = tf.nn.dropout(out, keep_prob=1 - dropout_d)
                 layers.append(kept)
 
         with tf.variable_scope("layer_%d" % (len(layers) + 1)):
             convolved = D_conv(layers[-1], out_channels=1)
             pooled = D_max_pool(convolved)
             fc1 = tf.contrib.layers.flatten(pooled)
-#            fc1 = tf.layers.dense(fc1, 100)
-#            fc1 = lrelu(fc1)
+            fc1 = tf.layers.dense(fc1, 100)
+            fc1 = lrelu(fc1)
             kept = tf.cond(training, lambda : tf.nn.dropout(fc1, 1 - dropout_d), lambda : fc1)
-            #if training:
-            #    fc1 = tf.nn.dropout(fc1, keep_prob=1 - dropout_d)
             out = tf.layers.dense(kept, 17)
             layers.append(out)
         return layers[-1]
@@ -199,31 +184,27 @@ def qc_discriminator(D_input, training):
                 out_channels = ndf/(2*(i+1))
                 convolved = D_conv(layers[-1], out_channels)
                 pooled = D_max_pool(convolved)
-                #normalized = batchnorm(pooled, training)
                 normalized = tf.cond(training, lambda : batchnorm(pooled, True), lambda : batchnorm(pooled, False))
                 out = lrelu(normalized, 0.1)
                 kept = tf.cond(training, lambda : tf.nn.dropout(out, keep_prob=1 - dropout_d), lambda : out)
-                #if training:
-                #    out = tf.nn.dropout(out, keep_prob=1 - dropout_d)
                 layers.append(kept)
 
         with tf.variable_scope("layer_%d" % (len(layers) + 1)):
             convolved = D_conv(layers[-1], out_channels=1)
             pooled = D_max_pool(convolved)
             fc1 = tf.contrib.layers.flatten(pooled)
-#            fc1 = tf.layers.dense(fc1, 100)
-#            fc1 = lrelu(fc1)
+            fc1 = tf.layers.dense(fc1, 100)
+            fc1 = lrelu(fc1)
             kept = tf.cond(training, lambda : tf.nn.dropout(fc1, 1 - dropout_d), lambda : fc1)
-            #if training:
-            #    fc1 = tf.nn.dropout(fc1, keep_prob=1 - dropout_d)
             out = tf.layers.dense(kept, 2)
             layers.append(out)
         return layers[-1]
     
 features = tf.placeholder(np.float32, [batch_size, 106, 128, 110])
+training = tf.placeholder(tf.bool, name='training')
+
 qc_labels = tf.placeholder(np.int32, [batch_size])
 site_labels = tf.placeholder(np.int32, [batch_size])
-training = tf.placeholder(tf.bool, name='training')
 
 with tf.variable_scope("generator"):
     debiased = generator(features, training)
@@ -253,9 +234,9 @@ qc_D_solver = tf.train.AdamOptimizer(lr, beta1)
 G_solver = tf.train.AdamOptimizer(lr, beta1)
 
 with tf.name_scope("G_loss"):
-    G_loss = Site_weight * tf.reduce_mean(
+    G_loss = -Site_weight * tf.reduce_mean(
         tf.nn.sparse_softmax_cross_entropy_with_logits(
-            logits=site_out, labels=site_labels)) - QC_weight * tf.reduce_mean(
+            logits=site_out, labels=site_labels)) + QC_weight * tf.reduce_mean(
             tf.nn.sparse_softmax_cross_entropy_with_logits(
             logits=qc_out, labels=qc_labels))
     G_loss_scalar = tf.summary.scalar('G_loss', G_loss)
@@ -299,7 +280,15 @@ def run_a_gan(sess, G_train_step, G_loss,\
                                 eval_src_folder="/home/smantra/finalproject/eval/",
                                 eval_cache_prefix="/home/smantra/finalproject/cache_eval/",
                                 batch_size=batch_size
-                         )
+                             )
+    test_ds = Dataset_Pipeline(target_shape=(106, 128, 110),
+                               n_epochs=1,
+                               train_src_folder="/home/smantra/finalproject/data/",
+                               train_cache_prefix="/home/smantra/finalproject/cache_train/",
+                               eval_src_folder="/home/smantra/finalproject/test/",
+                               eval_cache_prefix="/home/smantra/finalproject/cache_test/",
+                               batch_size=batch_size
+                              )
     train_dataset = _get_data(batch_size=train_ds.batch_size,
                               src_folder=train_ds.train_src_folder,
                               n_epochs=num_epoch,
@@ -316,7 +305,14 @@ def run_a_gan(sess, G_train_step, G_loss,\
                             target_shape=val_ds.target_shape,
                             balance_dataset=False
                            )
-
+    test_dataset = _get_data(batch_size=test_ds.batch_size,
+                              src_folder=test_ds.eval_src_folder,
+                              n_epochs=test_ds.n_epochs,
+                              cache_prefix=test_ds.eval_cache_prefix,
+                              shuffle=False,
+                              target_shape=test_ds.target_shape,
+                              balance_dataset=False
+                             )
 
     ds_it = train_dataset.make_initializable_iterator()
     tf.add_to_collection(tf.GraphKeys.TABLE_INITIALIZERS, ds_it.initializer)
@@ -325,10 +321,10 @@ def run_a_gan(sess, G_train_step, G_loss,\
 
     vds_it = val_dataset.make_one_shot_iterator()
     vds_batch = vds_it.get_next()
-    #vds_it = val_dataset.make_initializable_iterator()
-    #tf.add_to_collection(tf.GraphKeys.TABLE_INITIALIZERS, vds_it.initializer)
-    #sess.run(vds_it.initializer)
-    #vds_batch = vds_it.get_next()
+    
+    testds_it = test_dataset.make_one_shot_iterator()
+    test_batch = testds_it.get_next()
+ 
 
     ro = tf.RunOptions(report_tensor_allocations_upon_oom=True)
     print("Starting training:")
@@ -383,6 +379,16 @@ def run_a_gan(sess, G_train_step, G_loss,\
                 print('Val Accuracy: QC {:.4}, Site {:.4}'.format(qc_acc_curr, site_acc_curr))
                 print('QC Labels: {}, QC Predictions: {}'.format(vqc_labs, qc_p_curr))
                 print('Site Labels: {}, Site Predictions: {}'.format(vsite_labs, site_p_curr))
+    for test_step in range(math.floor(100/batch_size)):
+        test_feats, (test_qc_labs, test_site_labs) = sess.run(test_batch, options=ro)
+        test_feed_dict={features: test_feats,
+                        qc_labels : test_qc_labs,
+                        site_labels : test_site_labs,
+                        training : False
+                       }
+        qc_acc_curr, site_acc_curr, qc_p_curr, site_p_curr = sess.run([qc_acc, site_acc, qc_preds, site_preds], 
+                                                                      feed_dict=test_feed_dict, options=ro)
+        save_preds(test_step, test_qc_labs, test_site_labs, qc_p_curr, site_p_curr, test_out_file)
 
 def get_session():
     config = tf.ConfigProto()
@@ -400,4 +406,4 @@ with get_session() as sess:
               qc_D_train_step, qc_D_loss,\
               site_D_train_step, site_D_loss,\
               G_extra_step, qc_D_extra_step, site_D_extra_step,\
-              10, train_writer, test_writer)
+              5, train_writer, test_writer)
